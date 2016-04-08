00c8e4b3f7fa6c208bfa6f21a235983d130ab28a
Joshua Logan <joshlogan32323@gmail.com>
2016-04-08 11:50:38 -0700

    inital commit, basic JS objects created

---
+++ code-snippets.js
@@ -0,0 +1,32 @@
+$(".deliveryBtn").click(function(event) {
+  event.preventDefault();
+
+  $("#deliveryAddress").append('<form>' + '<div class="new-address">' +
+                               '<div class="form-group">' +
+                                 '<label for="new-street">Street</label>' +
+                                 '<input type="text" class="form-control new-street">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-city">City</label>' +
+                                 '<input type="text" class="form-control new-city">' +
+                               '</div>' +
+                               '<div class="form-group">' +
+                                 '<label for="new-state">State</label>' +
+                                 '<input type="text" class="form-control new-state">' +
+                               '</div>' +
+                             '</div>' + '<button type="submit" id="orderDeliveryButton">ORDER DELIVERY</button>' + '</form>')
+
+})
+
+$("form#orderDeliveryButton").submit(function(event) {
+  event.preventDefault();
+
+  var inputtedStreet = $(".new-street").val();
+  var inputtedCity = $(".new-city").val();
+  var inputtedState = $(".new-state").val();
+
+  var newAddress = new Delivery(inputtedStreet, inputtedCity, inputtedState);
+
+  console.log(inputtedStreet);
+
+})
---
+++ js/scripts.js
@@ -0,0 +1,79 @@
+function Pizza (size, sauce, meat, veggies) {
+  this.size = size
+  this.sauce = sauce
+  this.meat = meat
+  this.veggies = veggies
+}
+Pizza.prototype.calculatePrice = function () {
+  var price = 0
+
+  switch (this.size) {
+
+    case "Personal":
+      price = 5
+      break;
+    case "Small":
+      price = 10
+      break;
+    case "Medium":
+      price = 13
+      break;
+    case "Large":
+      price = 15
+      break;
+  }
+  if (this.sauce === "Pesto" || this.sauce === "BBQ") {
+    price += 1
+  }
+  if (this.meat === "MeatBalls" || this.meat === "Bacon" || this.meat === "Salami") {
+    price += 1
+  }
+  if (this.veggies === "Jalapenos" || this.veggies === "Bannana Peppers") {
+    price += 1
+  }
+  return price;
+}
+Pizza.prototype.displayPizza = function () {
+  return "Your Pizza: " + "<br>" + "<br>" + "Size: " + this.size + "<br>" + "Sauce: " + this.sauce + "<br>" + "Meat: " + this.meat + "<br>" + "Veggies: " + this.veggies
+}
+Pizza.prototype.delivery = function (street, city, state){
+  this.street = street
+  this.city = city
+  this.state = state
+  return "Delivery to: " + "<br>" + "<br>" + this.street + "<br>" + this.city + "<br>" + this.state
+}
+$(document).ready(function() {
+  $("form").submit(function(event) {
+    event.preventDefault();
+
+    var inputtedSize = $("#size").val()
+    var inputtedSauce = $("#sauce").val()
+    var inputtedMeat = $("#meat").val()
+    var inputtedVeggies = $("#veggies").val()
+
+    if (inputtedSize === null || inputtedSauce === null || inputtedMeat === null || inputtedVeggies === null) {
+      alert("Please enter a size and some toppings to order a pizza.")
+    } else {
+      var newPizza = new Pizza (inputtedSize, inputtedSauce, inputtedMeat, inputtedVeggies);
+      var pizzaPrice = newPizza.calculatePrice();
+
+
+      $("#results").prepend("<h3>" + newPizza.displayPizza() + "<br>" + "<br>" + "Price: $" + pizzaPrice + ".00" + "</h3>");
+      $(".pizza").show();
+      $("#orderForm").hide();
+
+    }
+  })
+
+  $(".orderBtn").click(function(event) {
+    event.preventDefault();
+
+    $("#orderForm").show();
+    $(".welcome").hide();
+  })
+
+
+
+
+
+});
